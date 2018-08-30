92e2720d6808edf7c6ec4649c5a5dd3da62ab22c
DESKTOP-DKK6M9Q\nerdfloor <kawanabe@gmail.com>
2018-08-31 01:48:07 +0900

    Nucleo_DCO_Test05: succes summing 3 OSC

--- mbed/Nucleo_DCO_Test05/src/main.cpp
+++ mbed/Nucleo_DCO_Test05/src/main.cpp
@@ -17,6 +17,8 @@
 #define TITLE_STR1  ("DCO Test05")
 #define TITLE_STR2  ("2018.08.31")
 
+#define OSC_NUM  (3)
+
 // Pin Assign
 AnalogOut Dac1(PA_4);
 AnalogOut Dac2(PA_5);
@@ -30,16 +32,16 @@ DigitalOut CheckPin2(D5);
 uint8_t u8x8_gpio_and_delay_mbed(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 u8g2_t U8g2Handler;
 
-// Parameter
-double drate = 1000.0;         // initial output rate (Hz)
-const double refclk = 100000;  // 100kHz
-
 // Interruput
 Ticker ticker;
 
 // DDS
-volatile uint32_t phaccu;
-volatile uint32_t tword_m;
+const double refclk = 100000;  // 100kHz
+double drate[OSC_NUM]          // output rate (Hz)
+	= { 1000.0, 2000.0, 3000.0 };
+	
+volatile uint32_t phaccu[OSC_NUM];
+volatile uint32_t tword_m[OSC_NUM];
 
 //-------------------------------------------------------------------------------------------------
 // Interrupt Service Routine
@@ -70,11 +72,18 @@ void update()
     CheckPin1.write(1);
 #endif
 
-    phaccu = phaccu + tword_m;
-    uint16_t idx = phaccu >> 17;  // use upper 15 bits
-    
-    internalDacWrite(1, sin_12bit_32k[idx]);
-    //internalDacWrite(2, sin_12bit_32k[idx]);
+	uint32_t sum = 0;
+	for (int i = 0; i < OSC_NUM; i++) {
+		phaccu[i] = phaccu[i] + tword_m[i];
+		uint16_t idx = phaccu[i] >> 17;  // use upper 15 bits
+
+		// Mix
+		sum += sin_12bit_32k[idx];
+	}
+	
+	sum = sum / OSC_NUM;
+
+	internalDacWrite(1, (uint16_t)sum);
 
 #if (PIN_CHECK)
     CheckPin1.write(0);
@@ -108,7 +117,9 @@ int main()
 	printf("CLOCKS_PER_SEC: %ld\n", CLOCKS_PER_SEC); 
 #endif
 
-    tword_m = pow(2.0, 32) * drate / refclk;  // calculate DDS tuning word;
+	for (int i = 0; i < OSC_NUM; i++) {
+		tword_m[i] = pow(2.0, 32) * drate[i] / refclk;  // calculate DDS tuning word;
+	}
     
     // 1.0s / 1.0us = 1000000.0
     float interruptPeriodUs = 1000000.0 / refclk; 
